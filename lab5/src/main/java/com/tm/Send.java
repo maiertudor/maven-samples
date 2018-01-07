d18113406ff8375638e0cc9c5a1fc703815165fa
TudorM <maiert69@gmail.com>
2018-01-07 21:21:33 +0200

    lab 5

---
+++ lab5/src/main/java/com/tm/Send.java
@@ -0,0 +1,32 @@
+package com.tm;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.Scanner;
+
+/**
+ * Created by tudor.maier on 07/01/2018.
+ */
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+
+        channel.close();
+        connection.close();
+    }
+}
